f908b2b462d07418714c40d4753f37e8189c861c
NathStevo97 <natstephenson15@gmail.com>
2021-09-29 22:13:25 +0100

    Higher or Lower and Rock Paper Scissors Examples

---
+++ Guess the Number/main.py
@@ -0,0 +1,27 @@
+from os import terminal_size
+import random
+
+rangeMin = int(input("\n Enter the lower number for the range: "))
+rangeMax = int(input("\n Enter the higher number for the range: "))
+
+number = random.randint(rangeMin - 1, rangeMax + 1)
+tries = 5
+guesses = []
+while tries > 0:
+    guess = int(input(f"\n Please Guess a Number between {rangeMin} and {rangeMax} \n"))
+    if guess < rangeMin or guess > rangeMax:
+        print("\n That number is outside of the range, please re-enter a number between {rangeMin} and {rangeMax} \n")
+    elif str(guess) in guesses:
+        print("\n That number has already been guessed, please try again! \n")
+    elif guess == number:
+        print(f"\n Correct! Your guess {guess} was the secret number. You guessed it with {tries} tries left!")
+        break
+    elif guess < number:
+        guesses.append(str(guess))
+        tries-=1
+        print(f"\n Your guess {guess} is below the secret number, you have {tries} tries left!")
+    elif guess > number:
+        guesses.append(str(guess))
+        tries-=1
+        print(f"\n Your guess {guess} is above the secret number, you have {tries} tries left!")
+print(f"\n Game over! The secret number was {number}")
---
+++ Rock Paper Scissors/main.py
@@ -0,0 +1,33 @@
+import random
+
+positions = ["rock", "paper", "scissors"]
+compPos = positions[random.randint(0,2)]
+
+gameActive = True
+
+while gameActive == True:
+    p1Pos = input("\n rock, paper, or scissors? ")
+    if p1Pos == compPos:
+        print("\n It's a tie!")
+    elif p1Pos == "rock":
+        if compPos == "paper":
+            print("\n You Lose! Paper Covers Rock!")
+        else:
+            print("You Win! Rock Breaks Scissors!")
+    elif p1Pos == "scissors":
+        if compPos == "rock":
+            print("\n You Lose! Rock Breaks Scissors!")
+        else:
+            print("You Win! Scissors Cuts Paper!")
+    elif p1Pos == "paper":
+        if compPos == "scissors":
+            print("\n You Lose! Scissors Cuts Paper!")
+        else:
+            print("You Win! Paper Covers Rock!")
+
+    playAgain = str(input("\n Would You Like to Play Again? Y or N? "))
+    if playAgain == "Y":
+        continue
+    else:
+        gameActive == False
+    compPos = positions[random.randint(0,2)]
