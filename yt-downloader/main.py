e1265cff7d4eb89ce425bd7dc17d590b35bb6edd
NathStevo97 <natstephenson15@gmail.com>
2023-06-03 23:10:49 +0100

    YT Downloader Updated with Progress Bar and OAUTH

--- yt-downloader/main.py
+++ yt-downloader/main.py
@@ -1,7 +1,8 @@
 from pytube import YouTube
+from pytube.cli import on_progress
 
 def Download(link):
-    youtubeObject = YouTube(link)
+    youtubeObject = YouTube(link, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
         youtubeObject.download()
